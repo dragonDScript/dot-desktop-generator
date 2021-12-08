caa39b41db3f7213946d10a431cb015359ce2d13
dragonDScript <agomila96@gmail.com>
2021-12-08 19:31:55 +0100

    Fix things

---
+++ README.md
@@ -0,0 +1,6 @@
+# .desktop generator
+
+Generates a .desktop file from a binary file, a title and an image.
+
+# 🚧 Warning: Bad code
+## I still don't know much Rust, so this is a very messy code
--- src/window.rs
+++ src/window.rs
@@ -1,11 +1,12 @@
-use glib::GString;
+use std::path::Path;
+use std::env;
+use std::io::Write;
+use std::fs::File;
 use gtk::prelude::*;
 
 pub struct Window {
     pub widget: gtk::ApplicationWindow,
 }
-
-//const PATH: &str = "~/.local/share/applications/";
 
 impl Window {
     pub fn new() -> Self {
@@ -13,6 +14,9 @@ impl Window {
         let widget: gtk::ApplicationWindow = builder
             .get_object("window")
             .expect("err: failed to find the window object");
+        let title_entry: gtk::Entry = builder
+            .get_object("titleInput")
+            .expect("Failed to find the titleInput object");
         let img_picker: gtk::FileChooserButton = builder
             .get_object("imgPicker")
             .expect("err: failed to find the imgPicker object");
@@ -24,17 +28,38 @@ impl Window {
             .expect("err: failed to find the btnSend object");
 
         btn_send.connect_clicked(move |btn_send| {
-            let title_entry: gtk::Entry = builder
-                .get_object("titleInput")
-                .expect("Failed to find the titleInput object");
-            let title_result = title_entry.get_text();
-            let mut title: &str = "";
-            match title_result {
-                Some(title) => {
+            match title_entry.get_text() {
+                Some(gstr_title) => {
+                    let title: String = String::from(gstr_title);
+
+                    match bin_picker.get_title() {
+                        Some(gstr_bin) => {
+                            let bin = String::from(gstr_bin);
+                            // format
+                            let path = format!("/home/{}/.local/share/applications", env::var("USER").unwrap());
+                            let mut formatted: String = format!("[Desktop Entry]
+Version=1.0
+Type=Application
+Name={title}
+Exec={binary}", title=title, binary=bin);
+                            let path = Path::new(&path);
+                            let path = &path.join(format!("{}.desktop", title));
+                            match File::create(path) {
+                                Ok(mut file) => {
+                                    if img_picker.get_title().is_some() {
+                                        formatted = format!("{}{}", formatted, format!("\nIcon={}", img_picker.get_title().unwrap()));
+                                    }
+                                    file.write_all(formatted.as_bytes()).expect("err: failed to write to file");
+                                },
+                                Err(err) => println!("err: failed to create file {}", err)
+                            }
+                        }
+                        None => println!("err: cannot get_title()")
+                    }
+
                 },
                 None => println!("err: cannot get_text()"),
             }
-
         });
 
         Self { widget }
